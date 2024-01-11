cd4917af8fe28539c749327e0bfd63d75512de6a
Wen Hai <107683607+wenhai-oh@users.noreply.github.com>
2024-01-11 12:40:56 +0800

    create user model & admin view
    1. User model created and migrated to DB server.
    2. Added User model to admin view.
    3. New superuser will need to be created. Also have to manually update the new superuser with is_superuser=True.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    # list of built-in methods: https://docs.djangoproject.com/en/5.0/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    company = models.CharField(max_length=100)
+    interests = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    contact_number = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'company', 'interests', 'contact_number']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_active and (self.is_superuser or self.is_staff)
+
+    def has_module_perms(self, app_label):
+        return self.is_active and (self.is_superuser or self.is_staff)
