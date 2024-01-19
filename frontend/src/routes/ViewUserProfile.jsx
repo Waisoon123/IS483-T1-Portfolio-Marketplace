7e55f4da9cc5bf496226c2ea4bee8b26c7357920
Wen Hai <107683607+wenhai-oh@users.noreply.github.com>
2024-01-19 21:34:15 +0800

    format document with prettier

--- frontend/src/routes/ViewUserProfile.jsx
+++ frontend/src/routes/ViewUserProfile.jsx
@@ -4,35 +4,38 @@ import axios from 'axios';
 import styles from './ViewUserProfile.module.css';
 
 const ViewUserProfile = () => {
-    const [userProfile, setUserProfile] = useState(null);
+  const [userProfile, setUserProfile] = useState(null);
 
-    useEffect(() => {
-        // Fetch user profile data from API
-        axios.get('http://localhost:8000/api/users/1')
-            .then(response => {
-                setUserProfile(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching user profile:', error);
-            });
-    }, []);
+  useEffect(() => {
+    // Fetch user profile data from API
+    axios
+      .get('http://localhost:8000/api/users/1')
+      .then(response => {
+        setUserProfile(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching user profile:', error);
+      });
+  }, []);
 
-    console.log(userProfile)
-    return (
-        <div className={styles.container}>
-            {userProfile ? (
-                <div>
-                    <h2>{userProfile.first_name} {userProfile.last_name}</h2>
-                    <p>Email: {userProfile.email}</p>
-                    <p>Company: {userProfile.company}</p>
-                    <p>Interests: {userProfile.interests}</p>
-                    <p>Contact Number: {userProfile.contact_number}</p>
-                </div>
-            ) : (
-                <p>Loading user profile...</p>
-            )}
+  console.log(userProfile);
+  return (
+    <div className={styles.container}>
+      {userProfile ? (
+        <div>
+          <h2>
+            {userProfile.first_name} {userProfile.last_name}
+          </h2>
+          <p>Email: {userProfile.email}</p>
+          <p>Company: {userProfile.company}</p>
+          <p>Interests: {userProfile.interests}</p>
+          <p>Contact Number: {userProfile.contact_number}</p>
         </div>
-    );
+      ) : (
+        <p>Loading user profile...</p>
+      )}
+    </div>
+  );
 };
 
 export default ViewUserProfile;
