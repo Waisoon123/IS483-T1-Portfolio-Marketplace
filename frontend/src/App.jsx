273bcccf62a9211b5641088c34d52e068639a268
Wen Hai <107683607+wenhai-oh@users.noreply.github.com>
2024-01-30 18:36:40 +0800

    Update Navbar display depending on user or visitor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Navbar isAuthenticated={isAuthenticated} />
+      <Outlet isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
     </>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,11 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
-function Navbar() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+function Navbar({ isAuthenticated }) {
   return (
-    // navbar
     <div className={styles.navbar}>
       <header className='App-header'>
         <nav className={styles.navbarBody}>
@@ -30,15 +27,6 @@ function Navbar() {
               </Link>
             </>
           )}
-          {/* <Link to='/login' className={styles.loginButton}>
-            Login
-          </Link>
-          <Link to='/sign-up' className={styles.signupButton}>
-            Sign Up
-          </Link>
-          <Link to='/profile' className={styles.loginButton}>
-            View User Profile
-          </Link> */}
         </nav>
       </header>
     </div>
