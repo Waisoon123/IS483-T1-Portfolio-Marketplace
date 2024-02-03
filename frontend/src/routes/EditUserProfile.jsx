2d390ed41bb690034514c78aca5e0d6afe05e4ad
Frqrs <114128043+Frqrs@users.noreply.github.com>
2024-02-04 03:08:14 +0800

    Update EditUserProfile Frontend

--- frontend/src/routes/EditUserProfile.jsx
+++ frontend/src/routes/EditUserProfile.jsx
@@ -230,191 +230,192 @@ function EditUserProfile() {
   };
 
   return (
-    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
-      <div className='w-50 rounded shadow-md bg-primary text-black px-8 pt-6 pb-8 mb-4'>
-        <Modal isOpen={isErrorModalOpen}>
-          <div>
-            <p>Please Login to Continue</p>
-            <button onClick={() => navigate(paths.LOGIN)}>Login</button>
-          </div>
-        </Modal>
-        <Modal isOpen={isSuccessModalOpen}>
-          <div>
-            <p>Update was successful!</p>
-            <button onClick={() => navigate(paths.VIEW_USER_PROFILE)}>Continue to View Profile</button>
-          </div>
-        </Modal>
-        <form onSubmit={handleSubmit(handleUpdate)}>
-          <div className='my-2'>
-            <label htmlFor={formFields.firstName} className='mr-2'>
-              {fromLabels.FIRST_NAME}
-            </label>
-            <input
-              {...register(formFields.firstName, { required: errorMessages.EMPTY_FIRST_NAME_ERROR_MESSAGE })}
-              type='text'
-              id={formFields.firstName}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.firstName}
-              value={firstName}
-              placeholder='First Name'
-              onChange={e => setFirstName(e.target.value)}
-            />
-            <p className={styles.errorMsg}>
-              {errors[formFields.firstName] ? errors[formFields.firstName].message : ''}
-            </p>
-          </div>
-          <div className='mb-2'>
-            <label htmlFor={formFields.lastName} className='mr-2'>
-              {fromLabels.LAST_NAME}
-            </label>
-            <input
-              {...register(formFields.lastName, { required: errorMessages.EMPTY_LAST_NAME_ERROR_MESSAGE })}
-              type='text'
-              id={formFields.lastName}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.lastName}
-              value={lastName}
-              placeholder='Last Name'
-              onChange={e => setLastName(e.target.value)}
-            />
-            <p className={styles.errorMsg}>{errors[formFields.lastName] ? errors[formFields.lastName].message : ''}</p>
-          </div>
-          <div className='mb-2'>
-            <label htmlFor={formFields.email} className='mr-11'>
-              {fromLabels.EMAIL}
-            </label>
-            <input
-              {...register(formFields.email, { required: errorMessages.EMPTY_EMAIL_ERROR_MESSAGE })}
-              type='text'
-              id={formFields.email}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.email}
-              value={email}
-              placeholder='Email'
-              onChange={e => setEmail(e.target.value)}
-            />
-            <p className={styles.errorMsg}>{errors[formFields.email] ? errors[formFields.email].message : ''}</p>
-          </div>
-          <div className='mb-2'>
-            <label htmlFor={formFields.company} className='mr-3'>
-              {fromLabels.COMPANY}
-            </label>
-            <input
-              {...register(formFields.company, { required: errorMessages.EMPTY_COMPANY_ERROR_MESSAGE })}
-              type='text'
-              id={formFields.company}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.company}
-              value={company}
-              placeholder='Company'
-              onChange={e => setCompany(e.target.value)}
-            />
-            <p className={styles.errorMsg}>{errors[formFields.company] ? errors[formFields.company].message : ''}</p>
-          </div>
-          <div className='mb-2'>
-            <label htmlFor={formFields.interests} className='mr-5'>
-              {fromLabels.INTERESTS}
-            </label>
-            <input
-              {...register(formFields.interests, { required: errorMessages.EMPTY_INTERESTS_ERROR_MESSAGE })}
-              type='text'
-              id={formFields.interests}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.interests}
-              value={interests}
-              placeholder='Interests'
-              onChange={e => setInterests(e.target.value)}
-            />
-            <p className={styles.errorMsg}>
-              {errors[formFields.interests] ? errors[formFields.interests].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.contactNumber}>{fromLabels.CONTACT_NUMBER}</label>
-            <Controller
-              control={control}
-              name={formFields.contactNumber}
-              defaultValue={contactNumber}
-              rules={{ required: errorMessages.EMPTY_CONTACT_NUMBER_ERROR_MESSAGE }}
-              render={({ field }) => (
-                <PhoneInput
-                  id={formFields.contactNumber}
-                  className={`${formFields.contactNumber} ${styles.input}`}
-                  value={field.value}
-                  onChange={value => {
-                    setContactNumber(value);
-                    field.onChange(value);
-                  }}
-                  placeholder='Enter contact number'
-                  defaultCountry='SG'
-                  international
-                  {...field}
-                />
-              )}
-            />
-            <p className={styles.errorMsg}>
-              {errors[formFields.contactNumber] ? errors[formFields.contactNumber].message : ''}
-            </p>
-          </div>
-          <div className='my-2'>
-            <input
-              type='checkbox'
-              id='updatePasswordCheckbox'
-              checked={updatePassword}
-              onChange={handlePasswordCheckboxChange}
-              className='accent-green'
-            />
-            <label htmlFor='updatePasswordCheckbox'> Update Password</label>
-          </div>
-          <div className='mb-2'>
-            <label htmlFor={formFields.password} className='mr-5'>
-              {fromLabels.PASSWORD}
-            </label>
-            <input
-              type='password'
-              id={formFields.password}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.password}
-              placeholder='Password'
-              disabled={!updatePassword} // Disable if updatePassword is false
-              {...register(formFields.password)}
-            />
-            <p className={styles.errorMsg}>{errors[formFields.password] ? errors[formFields.password].message : ''}</p>
-          </div>
-          <div className='mb-2 mr-14'>
-            <label htmlFor={formFields.confirmPassword} className='mr-4'>
-              {fromLabels.CONFIRM_PASSWORD}
-            </label>
-            <input
-              type='password'
-              id={formFields.confirmPassword}
-              className='shadow appearance-none rounded w-50 py-2 px-3 leading-tight focus:shadow-outline'
-              name={formFields.confirmPassword}
-              placeholder='Confirm Password'
-              disabled={!updatePassword} // Disable if updatePassword is false
-              {...register(formFields.confirmPassword)}
-            />
-            <p className={styles.errorMsg}>
-              {errors[formFields.confirmPassword] ? errors[formFields.confirmPassword].message : ''}
-            </p>
-          </div>
-          <div className='mb-2'>
-            <button
-              type='button'
-              className='inline-block align-baseline border bg-red hover:bg-button-hoverred text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline'
-              onClick={handleCancel}
-            >
-              Cancel
-            </button>
-            <button
-              type='submit'
-              className='inline-block align-baseline border bg-green hover:bg-button-hovergreen text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline'
-            >
-              Update
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className={styles.container}>
+      <Modal isOpen={isErrorModalOpen}>
+        <div>
+          <p>Please Login to Continue</p>
+          <button onClick={() => navigate(paths.LOGIN)}>Login</button>
+        </div>
+      </Modal>
+      <Modal isOpen={isSuccessModalOpen}>
+        <div>
+          <p>Update was successful!</p>
+          <button onClick={() => navigate(paths.VIEW_USER_PROFILE)}>Continue to View Profile</button>
+        </div>
+      </Modal>
+      <form onSubmit={handleSubmit(handleUpdate)} className={styles.form}>
+        <div className={styles.field}>
+          <label htmlFor={formFields.firstName} className={styles.title}>
+            {fromLabels.FIRST_NAME}
+          </label>
+          <input
+            {...register(formFields.firstName, { required: errorMessages.EMPTY_FIRST_NAME_ERROR_MESSAGE })}
+            type='text'
+            id={formFields.firstName}
+            className={styles.input}
+            name={formFields.firstName}
+            value={firstName}
+            placeholder='First Name'
+            onChange={e => setFirstName(e.target.value)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.firstName] ? errors[formFields.firstName].message : ''}</p>
+
+          <label htmlFor={formFields.lastName} className={styles.title}>
+            {fromLabels.LAST_NAME}
+          </label>
+          <input
+            {...register(formFields.lastName, { required: errorMessages.EMPTY_LAST_NAME_ERROR_MESSAGE })}
+            type='text'
+            id={formFields.lastName}
+            className={styles.input}
+            name={formFields.lastName}
+            value={lastName}
+            placeholder='Last Name'
+            onChange={e => setLastName(e.target.value)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.lastName] ? errors[formFields.lastName].message : ''}</p>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.email} className={styles.title}>
+            {fromLabels.EMAIL}
+          </label>
+          <input
+            {...register(formFields.email, { required: errorMessages.EMPTY_EMAIL_ERROR_MESSAGE })}
+            type='text'
+            id={formFields.email}
+            className={styles.input}
+            name={formFields.email}
+            value={email}
+            placeholder='Email'
+            onChange={e => setEmail(e.target.value)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.email] ? errors[formFields.email].message : ''}</p>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.company} className={styles.title}>
+            {fromLabels.COMPANY}
+          </label>
+          <input
+            {...register(formFields.company, { required: errorMessages.EMPTY_COMPANY_ERROR_MESSAGE })}
+            type='text'
+            id={formFields.company}
+            className={styles.input}
+            name={formFields.company}
+            value={company}
+            placeholder='Company'
+            onChange={e => setCompany(e.target.value)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.company] ? errors[formFields.company].message : ''}</p>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.interests} className={styles.title}>
+            {fromLabels.INTERESTS}
+          </label>
+          <input
+            {...register(formFields.interests, { required: errorMessages.EMPTY_INTERESTS_ERROR_MESSAGE })}
+            type='text'
+            id={formFields.interests}
+            className={styles.input}
+            name={formFields.interests}
+            value={interests}
+            placeholder='Interests'
+            onChange={e => setInterests(e.target.value)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.interests] ? errors[formFields.interests].message : ''}</p>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.contactNumber} className={styles.title}>
+            {fromLabels.CONTACT_NUMBER}
+          </label>
+          <Controller
+            control={control}
+            name={formFields.contactNumber}
+            defaultValue={contactNumber}
+            rules={{ required: errorMessages.EMPTY_CONTACT_NUMBER_ERROR_MESSAGE }}
+            render={({ field }) => (
+              <PhoneInput
+                id={formFields.contactNumber}
+                className={`${formFields.contactNumber} ${styles.phoneinput}`}
+                value={field.value}
+                onChange={value => {
+                  setContactNumber(value);
+                  field.onChange(value);
+                }}
+                placeholder='Enter contact number'
+                defaultCountry='SG'
+                international
+                {...field}
+              />
+            )}
+          />
+          <p className={styles.errorMsg}>
+            {errors[formFields.contactNumber] ? errors[formFields.contactNumber].message : ''}
+          </p>
+        </div>
+
+        <div className={styles.field}>
+          <input
+            type='checkbox'
+            id='updatePasswordCheckbox'
+            checked={updatePassword}
+            onChange={handlePasswordCheckboxChange}
+            className={styles.checkbox}
+          />
+          <label htmlFor='updatePasswordCheckbox' className={styles.title}>
+            {' '}
+            Update Password
+          </label>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.password} className={styles.title}>
+            {fromLabels.PASSWORD}
+          </label>
+          <input
+            type='password'
+            id={formFields.password}
+            className={styles.input}
+            name={formFields.password}
+            placeholder='Password'
+            disabled={!updatePassword} // Disable if updatePassword is false
+            {...register(formFields.password)}
+          />
+          <p className={styles.errorMsg}>{errors[formFields.password] ? errors[formFields.password].message : ''}</p>
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor={formFields.confirmPassword} className={styles.title}>
+            {fromLabels.CONFIRM_PASSWORD}
+          </label>
+          <input
+            type='password'
+            id={formFields.confirmPassword}
+            className={styles.input}
+            name={formFields.confirmPassword}
+            placeholder='Confirm Password'
+            disabled={!updatePassword} // Disable if updatePassword is false
+            {...register(formFields.confirmPassword)}
+          />
+          <p className={styles.errorMsg}>
+            {errors[formFields.confirmPassword] ? errors[formFields.confirmPassword].message : ''}
+          </p>
+        </div>
+
+        <div>
+          <button type='submit' className={styles.cfmButton}>
+            Update
+          </button>
+        </div>
+        <div>
+          <button type='button' className={styles.cancelButton} onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
