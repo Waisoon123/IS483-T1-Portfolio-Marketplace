c765a6e56ab67e21451e179e31aad03ad3bde325
joelohkaishunhiroyuki <joel.oh.2021@smu.edu.sg>
2024-02-11 15:51:49 +0800

    Create Profile Model (Added Import Export)
    Addition of Import & Export on admin interface to import or export excel files to populate the database

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import User, Company
+from import_export.admin import ImportExportModelAdmin
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'last_name', 'company', 'interests')
     
-class CompanyAdmin(admin.ModelAdmin):
+class CompanyAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('company', 'description', 'tech_sector', 'hq_main_office', 'vertex_entity', 'finance_stage', 'status', 'website')
     
 admin.site.register(User, UserAdmin)
