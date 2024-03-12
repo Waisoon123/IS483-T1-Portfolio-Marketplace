97b9b94545965a5beb10c0a68b7ddce52c7c2e42
Waisoon123 <waisoon1999@gmail.com>
2024-03-12 22:09:44 +0800

    Update Navbar.jsx
    [Added] Vertex transparent logo to assets folder for logo brand.
    
    [Added] Input icons for revamped navbar UI styling.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../App.jsx';
+import logo from '../assets/vertex_holdings_logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
@@ -15,67 +18,67 @@ function Navbar() {
   };
 
   return (
-    <nav className='sticky top-0 z-50 h-16 bg-secondary-200 text-white'>
-      <div className='container mx-auto flex justify-between items-center h-full'>
+    <nav className='sticky top-0 z-50 h-20 bg-secondary-200 text-white'>
+      <div className='container mx-auto flex justify-evenly items-center h-full'>
         <div>
-          {/* <Brand /> */}
-          LOGO
+          <img src={logo} className='w-32 h-32' />
         </div>
-        <div className='flex justify-between text-md sm:text-sm md:text-base lg:text-base'>
-          <ul className='flex justify-between list-none'>
+        <div className='flex text-md sm:text-sm md:text-base lg:text-base'>
+          <ul className='flex list-none font-semibold'>
             <li className='mr-8'>
-              <Link to=''>PlaceHolder</Link>
+              <Link to=''>Home</Link>
             </li>
             <li className='mr-8'>
-              <Link to=''>PlaceHolder</Link>
+              <Link to=''>About</Link>
             </li>
             <li>
-              <Link to=''>PlaceHolder</Link>
+              <Link to=''>Directory</Link>
             </li>
           </ul>
-        </div>
 
-        <div className='flex justify-between items-center list-none'>
-          {isAuthenticated ? (
-            <>
-              <li className='mr-8'>
-                <Link
-                  to='/profile'
-                  className='text-black font-semibold bg-secondary-100 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
-                >
-                  View User Profile
-                </Link>
-              </li>
+          <div className='flex justify-end items-center list-none ml-8'>
+            {isAuthenticated ? (
+              <>
+                <li className='mr-8'>
+                  <Link
+                    to='/profile'
+                    className='text-black font-semibold bg-secondary-100 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
+                  >
+                    View User Profile
+                  </Link>
+                </li>
 
-              <li>
-                <button
-                  className='text-black font-semibold bg-secondary-100 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className='mr-8'>
-                <Link
-                  to='/login'
-                  className='text-black font-semibold bg-secondary-100 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/sign-up'
-                  className='text-black font-semibold bg-secondary-100 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
-                >
-                  Sign Up
-                </Link>
-              </li>
-            </>
-          )}
+                <li>
+                  <button
+                    className='text-black font-semibold bg-secondary-100 border-white rounded py-4 px-6 sm:text-xs md:text-base lg:text-base'
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className='mr-8'>
+                  <Link
+                    to='/login'
+                    className='text-white font-semibold border-2 border-white rounded p-4 sm:text-xs md:text-base lg:text-base'
+                  >
+                    <FontAwesomeIcon icon={faUserCircle} className='mr-2' size='xl' />
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to='/sign-up'
+                    className='text-white font-semibold border-2 border-white rounded py-4 px-6 sm:text-xs md:text-base lg:text-base'
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </nav>
