40687dd717242f1da8b071c0257bc0440b4eaba1
Waisoon123 <waisoon1999@gmail.com>
2024-03-15 00:30:13 +0800

    Update SignUp.jsx
    [Revamp] UI styling for the SignUp Form.
    
    [Refactor] Error messages to apply on the left column instead of below the various different input fields.
    
    [Refactor] Added conditional header, to allow more clearer to actions.

--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -10,6 +10,8 @@ import * as errorMessages from '../constants/errorMessages';
 import * as paths from '../constants/paths.js';
 import * as fromLabels from '../constants/formLabelTexts.js';
 import Button from '../components/Button.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const API_URL = import.meta.env.VITE_API_URL;
 let FORM_DATA;
@@ -209,188 +211,182 @@ export default function SignUp() {
           </button>
         </div>
       </Modal>
-      <div className='fixed inset-0 bg-black bg-opacity-50 z-10'></div>
-      <form
-        method='post'
-        className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 mt-5 bg-primary p-8 rounded-lg h-auto'
-        onSubmit={handleSubmit(handleSignUp)}
-      >
-        <h1 className='text-2xl font-bold text-center mb-5'>Sign Up</h1>
-        <div className='flex flex-col justify-center items-center'>
-          <div>
-            <label htmlFor={formFields.firstName} className='sr-only'>
-              {fromLabels.FIRST_NAME}
-            </label>
-            <input
-              type='text'
-              id={formFields.firstName}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.firstName}
-              placeholder='First Name'
-              {...register(formFields.firstName, { required: errorMessages.FIRST_NAME_ERROR_MESSAGES.empty })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.firstName] ? errors[formFields.firstName].message : ''}
-            </p>
+      <div className='bg-primary px-32 py-24'>
+        <div className='flex'>
+          <div className='w-1/2 bg-secondary-100 px-12 py-12'>
+            <h1 className='text-black text-4xl font-semibold font-sans'>
+              {Object.keys(errors).length === 0
+                ? 'Thank you for signing up!'
+                : 'Uh Oh... Seems like something is wrong !'}
+            </h1>
+            {Object.values(errors).map((error, index) => (
+              <p key={index} className='text-red text-md mt-8'>
+                <FontAwesomeIcon icon={faTimes} className='mr-2' size='xl' />
+                {error.message}
+              </p>
+            ))}
           </div>
-          <div>
-            <label htmlFor={formFields.lastName} className='sr-only'>
-              {fromLabels.LAST_NAME}
-            </label>
-            <input
-              type='text'
-              id={formFields.lastName}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.lastName}
-              placeholder='Last Name'
-              {...register(formFields.lastName, { required: errorMessages.LAST_NAME_ERROR_MESSAGES.empty })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.lastName] ? errors[formFields.lastName].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.email} className='sr-only'>
-              {fromLabels.EMAIL}
-            </label>
-            <input
-              type='text'
-              id={formFields.email}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.email}
-              placeholder='Email'
-              {...register(formFields.email, { required: errorMessages.EMAIL_ERROR_MESSAGES.empty })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.email] ? errors[formFields.email].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.password} className='sr-only'>
-              {fromLabels.PASSWORD}
-            </label>
-            <input
-              type='password'
-              id={formFields.password}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.password}
-              placeholder='Password'
-              data-testid='password-input'
-              {...register(formFields.password, { required: errorMessages.PASSWORD_ERROR_MESSAGES.empty })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.password] ? errors[formFields.password].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.confirmPassword} className='sr-only'>
-              {fromLabels.CONFIRM_PASSWORD}
-            </label>
-            <input
-              type='password'
-              id={formFields.confirmPassword}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.confirmPassword}
-              placeholder='Confirm Password'
-              data-testid='confirm-password-input'
-              {...register(formFields.confirmPassword, {
-                required: errorMessages.CONFIRM_PASSWORD_ERROR_MESSAGES.empty,
-              })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.confirmPassword] ? errors[formFields.confirmPassword].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.company} className='sr-only'>
-              {fromLabels.COMPANY}
-            </label>
-            <input
-              type='text'
-              id={formFields.company}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
-              name={formFields.company}
-              placeholder='Company'
-              {...register(formFields.company, { required: errorMessages.COMPANY_ERROR_MESSAGES.empty })}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.company] ? errors[formFields.company].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.interests} className='sr-only'>
-              {fromLabels.INTERESTS}
-            </label>
-            <div className='flex flex-wrap gap-2'>
-              {selectedInterests.map(interest => (
-                <div
-                  data-testid={interest.name}
-                  key={interest.id}
-                  className='flex justify-center bg-secondary-300 text-white w-auto p-2 text-md font-medium mb-2.5 rounded-md'
-                >
-                  {interest.name}
-                  <button className='ml-2 cursor-pointer border-none' onClick={() => handleRemoveInterest(interest.id)}>
-                    &#x2715;
-                  </button>
+          <div className='w-1/2 bg-white px-20 py-12'>
+            <h1 className='text-black text-4xl font-semibold font-sans'>Create Account</h1>
+            <p className='mt-8 text-black text-lg'>All fields are mandatory, please kindly fill up.</p>
+            <form method='post' className='' onSubmit={handleSubmit(handleSignUp)}>
+              <div className='flex flex-col'>
+                <div className='flex space-x-4'>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.firstName} className='mt-8 mb-2 text-gray-700'>
+                      {fromLabels.FIRST_NAME}
+                    </label>
+                    <input
+                      type='text'
+                      id={formFields.firstName}
+                      className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
+                      name={formFields.firstName}
+                      {...register(formFields.firstName, { required: errorMessages.FIRST_NAME_ERROR_MESSAGES.empty })}
+                    />
+                  </div>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.lastName} className='mt-8 mb-2 text-gray-700'>
+                      {fromLabels.LAST_NAME}
+                    </label>
+                    <input
+                      type='text'
+                      id={formFields.lastName}
+                      className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
+                      name={formFields.lastName}
+                      {...register(formFields.lastName, { required: errorMessages.LAST_NAME_ERROR_MESSAGES.empty })}
+                    />
+                  </div>
                 </div>
-              ))}
-            </div>
-            <select data-testid="select-interest"
-              id={formFields.interests}
-              className='w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm text-gray-500 text-md'
-              name={formFields.interests}
-              placeholder='Interests'
-              defaultValue='' // Show empty option when no interest is selected.
-              {...register(formFields.interests)}
-            >
-              <option data-testid="select-option" value='' disabled hidden>
-                Choose an interest
-              </option>
-              {availableInterests.map(interest => (
-                <option key={interest.id} value={interest.id}>
-                  {interest.name}
-                </option>
-              ))}
-            </select>
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.interests] ? errors[formFields.interests].message : ''}
-            </p>
-          </div>
-          <div>
-            <label htmlFor={formFields.contactNumber} className='sr-only'>
-              {fromLabels.CONTACT_NUMBER}
-            </label>
-            <Controller
-              control={control}
-              name={formFields.contactNumber}
-              defaultValue=''
-              rules={{ required: errorMessages.CONTACT_NUMBER_ERROR_MESSAGES.empty }}
-              render={({ field }) => (
-                <PhoneInput
-                  id={formFields.contactNumber}
-                  className={`${formFields.contactNumber} w-[500px] h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md`}
-                  placeholder='Enter contact number'
-                  defaultCountry='SG'
-                  international
-                  {...field}
-                />
-              )}
-            />
-            <p className='mt-2.5 mb-2.5 font-medium text-sm text-red'>
-              {errors[formFields.contactNumber] ? errors[formFields.contactNumber].message : ''}
-            </p>
-          </div>
-          <div>
-            <Button
-              type='submit'
-              className='w-[500px] h-10 border-2 border-secondary-300 rounded-sm text-secondary-300 shadow-md hover:bg-secondary-300 hover:text-primary text-md font-bold'
-              onClick={() => {}} // No-op function
-            >
-              Sign Up
-            </Button>
+                <div className='flex flex-col'>
+                  <label htmlFor={formFields.email} className='mt-2 mb-2 text-gray-700'>
+                    {fromLabels.EMAIL}
+                  </label>
+                  <input
+                    type='text'
+                    id={formFields.email}
+                    className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
+                    name={formFields.email}
+                    {...register(formFields.email, { required: errorMessages.EMAIL_ERROR_MESSAGES.empty })}
+                  />
+                </div>
+                <div className='flex space-x-4'>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.password} className='mt-2 mb-2 text-gray-700'>
+                      {fromLabels.PASSWORD}
+                    </label>
+                    <input
+                      type='password'
+                      id={formFields.password}
+                      className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
+                      name={formFields.password}
+                      data-testid='password-input'
+                      {...register(formFields.password, { required: errorMessages.PASSWORD_ERROR_MESSAGES.empty })}
+                    />
+                  </div>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.confirmPassword} className='mt-2 mb-2 text-gray-700'>
+                      {fromLabels.CONFIRM_PASSWORD}
+                    </label>
+                    <input
+                      type='password'
+                      id={formFields.confirmPassword}
+                      className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md'
+                      name={formFields.confirmPassword}
+                      data-testid='confirm-password-input'
+                      {...register(formFields.confirmPassword, {
+                        required: errorMessages.CONFIRM_PASSWORD_ERROR_MESSAGES.empty,
+                      })}
+                    />
+                  </div>
+                </div>
+                <div className='flex space-x-4'>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.company} className='mt-2 mb-2 text-gray-700'>
+                      {fromLabels.COMPANY}
+                    </label>
+                    <input
+                      type='text'
+                      id={formFields.company}
+                      className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md mb-2'
+                      name={formFields.company}
+                      {...register(formFields.company, { required: errorMessages.COMPANY_ERROR_MESSAGES.empty })}
+                    />
+                  </div>
+                  <div className='flex flex-col w-1/2'>
+                    <label htmlFor={formFields.contactNumber} className='mt-2 mb-2 text-gray-700'>
+                      {fromLabels.CONTACT_NUMBER}
+                    </label>
+                    <Controller
+                      control={control}
+                      name={formFields.contactNumber}
+                      defaultValue=''
+                      rules={{ required: errorMessages.CONTACT_NUMBER_ERROR_MESSAGES.empty }}
+                      render={({ field }) => (
+                        <PhoneInput
+                          id={formFields.contactNumber}
+                          className={`${formFields.contactNumber} w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm placeholder-gray-500 placeholder-italic text-md`}
+                          defaultCountry='SG'
+                          international
+                          {...field}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col'>
+                  <label htmlFor={formFields.interests} className='mt-2 mb-2 text-gray-700'>
+                    {fromLabels.INTERESTS}
+                  </label>
+                  <div className='flex flex-wrap gap-2'>
+                    {selectedInterests.map(interest => (
+                      <div
+                        data-testid={interest.name}
+                        key={interest.id}
+                        className='flex justify-center bg-secondary-300 text-white w-auto p-2 text-md font-medium mb-2.5 rounded-md'
+                      >
+                        {interest.name}
+                        <button
+                          className='ml-2 cursor-pointer border-none'
+                          onClick={() => handleRemoveInterest(interest.id)}
+                        >
+                          &#x2715;
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                  <select
+                    data-testid='select-interest'
+                    id={formFields.interests}
+                    className='w-full h-[40px] pl-2.5 border border-secondary-300 rounded-sm text-gray-500 text-md'
+                    name={formFields.interests}
+                    placeholder='Interests'
+                    defaultValue='' // Show empty option when no interest is selected.
+                    {...register(formFields.interests)}
+                  >
+                    <option data-testid='select-option' value='' disabled hidden>
+                      Choose an interest
+                    </option>
+                    {availableInterests.map(interest => (
+                      <option key={interest.id} value={interest.id}>
+                        {interest.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <Button
+                    type='submit'
+                    className='mt-4 w-full bg-secondary-100 rounded-sm text-black text-md font-bold py-4'
+                    onClick={() => {}} // No-op function
+                  >
+                    Sign Up
+                  </Button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </form>
+      </div>
     </>
   );
 }
