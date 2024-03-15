cc60ac315d714b5518131d1155fca4c97aa034d4
Waisoon123 <waisoon1999@gmail.com>
2024-03-15 23:31:36 +0800

    Update Login.jsx
    [Create] Show / Hide password functionality
    
    [Add] Responsive design to the UI screen for Login.jsx

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useState, useContext } from 'react';
 import { AuthContext } from '../App.jsx';
@@ -6,10 +6,13 @@ import Modal from '../components/Modal';
 import * as fromLabels from '../constants/formLabelTexts.js';
 import Button from '../components/Button.jsx';
 import video from '../assets/Dots_Video_Vertex.mp4';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   const {
     handleSubmit,
     setValue,
@@ -101,16 +104,16 @@ export default function Login() {
               <source src={video} type='video/mp4' />
             </video>
           </div>
-          <div className='w-1/2 bg-white px-20 py-12'>
+          <div className='w-1/2 bg-white lg:px-20 lg:py-12 sm:px-4 sm:py-4'>
             <form onSubmit={handleSubmit(handleLogin)} className=''>
-              <h1 className='mt-16 text-4xl font-semibold font-sans'>Welcome Back</h1>
-              <div className='mt-16'>
+              <h1 className='mt-16 text-4xl font-semibold font-sans sm:text-md'>Welcome Back</h1>
+              <div className='mt-16 sm:mt-8'>
                 <label className='sr-only' htmlFor={formFields.email}>
                   {fromLabels.EMAIL}
                 </label>
                 <input
                   type='text'
-                  className='w-full p-2.5 border border-secondary-300 mt-2.5 rounded-sm'
+                  className='w-full p-2.5 border border-secondary-300 mt-2.5 rounded-sm sm:text-sm lg:text-md'
                   name={formFields.email}
                   placeholder='Email'
                   id={formFields.email}
@@ -118,40 +121,53 @@ export default function Login() {
                 />
                 {errors[formFields.email] && <p className='mt-2.5 font-medium text-sm text-red'>Email is required.</p>}
               </div>
-              <div className='mt-4 mb-4'>
+              <div className='mt-4 mb-4 relative'>
                 <label className='sr-only' htmlFor={formFields.password}>
                   {fromLabels.PASSWORD}
                 </label>
                 <input
-                  type='password'
-                  className='w-full p-2.5 border border-secondary-300 mt-2.5 rounded-sm'
+                  type={showPassword ? 'text' : 'password'}
+                  className='w-full p-2.5 border border-secondary-300 mt-2.5 rounded-sm sm:text-sm lg:text-md'
                   name={formFields.password}
                   placeholder='Password'
                   id={formFields.password}
                   {...register(formFields.password, { required: true })}
                 />
+                <div
+                  className='absolute inset-y-0 right-0 top-2 pr-3 flex items-center cursor-pointer'
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FontAwesomeIcon icon={faEyeSlash} className='text-secondary-200' />
+                  ) : (
+                    <FontAwesomeIcon icon={faEye} className='text-secondary-200' />
+                  )}
+                </div>
                 {errors[formFields.password] && (
                   <p className='mt-2.5 font-medium text-sm text-red'>Password is required.</p>
                 )}
               </div>
-              <div className='flex justify-between mb-4'>
-                <label className='flex items-center text-sm'>
+              <div className='flex sm:block lg:flex justify-between mb-4'>
+                <label className='flex items-center text-sm sm:mb-2'>
                   <input type='checkbox' className='mr-2' />
                   Remember me
                 </label>
-                <a href='/forgot-password' className='text-secondary-300 text-sm'>
+                <a href='/forgot-password' className='text-secondary-300 text-sm sm:text-xs'>
                   Forgot your password?
                 </a>
               </div>
-              <Button type='submit' className='w-full p-2.5 bg-secondary-100 rounded-sm text-black text-md font-bold'>
+              <Button
+                type='submit'
+                className='w-full p-2.5 bg-secondary-100 rounded-sm text-black text-md font-bold sm:text-sm'
+              >
                 Login
               </Button>
               <div className='mb-24'>
-                <p className='mt-4 text-center text-black'>
-                  Don't have an account?{' '}
-                  <a href='/signup' className='font-bold text-black underline'>
+                <p className='mt-4 text-center text-black sm:text-sm'>
+                  Don&apos;t have an account?{' '}
+                  <Link to='/sign-up' className='font-bold text-black underline sm:text-sm'>
                     Sign up
-                  </a>
+                  </Link>
                 </p>
               </div>
             </form>
