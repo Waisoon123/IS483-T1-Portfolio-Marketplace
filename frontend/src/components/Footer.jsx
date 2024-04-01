3e6073c72e5aba5d3b420c00098bca43548a56a6
Frqrs <114128043+Frqrs@users.noreply.github.com>
2024-04-01 09:49:07 +0800

    Added Footer
    Footer added with links to the different vertex entities. Malito link added to the email icon. The other social media icons point to the ones in the main vertex holdings website.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faLinkedin, faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import logo from '../assets/Vertex_holdings_logo-bg.png';
+
+function Footer() {
+  return (
+    <footer className='bg-secondary-200 text-white'>
+      <div className='container mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8'>
+        <div className='mb-6 md:mb-0'>
+          <img src={logo} alt='Vertex Holdings Logo' className='mb-4' />
+          <p className='text-white text-sm'>
+            The Vertex global network of venture capital funds comprises Vertex Ventures, Vertex Ventures HC and Vertex
+            Growth. With funds based across innovation hubs in China, Israel, Southeast Asia and India, and the US, we
+            create a unique platform for portfolio companies to realize their full potential by leveraging the combined
+            experience and resources of our extensive network of global partners.
+          </p>
+        </div>
+        <div className='mb-6 md:mb-0'>
+          <h5 className='uppercase mb-2 font-bold'>Entities</h5>
+          <a
+            href='https://www.vertexgrowth.com/'
+            className='block mb-2 text-sm'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vertex Growth (VGF)
+          </a>
+          <a
+            href='https://www.vertexventures.cn/en/'
+            className='block mb-2 text-sm'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vertex Ventures China (VVCN)
+          </a>
+          <a
+            href='https://www.vertexventureshc.com/'
+            className='block mb-2 text-sm'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vertex Ventures HC (VVHC)
+          </a>
+          <a
+            href='https://www.vertexventures.co.il/'
+            className='block mb-2 text-sm'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vertex Ventures Israel (VVIL)
+          </a>
+          <a
+            href='https://www.vertexventures.sg/'
+            className='block mb-2 text-sm'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vertex Ventures SEA & India (VVSEAI)
+          </a>
+          <a href='https://vvus.com/' className='block mb-2 text-sm' target='_blank' rel='noopener noreferrer'>
+            Vertex Ventures US (VVUS)
+          </a>
+          {/* Additional links */}
+        </div>
+        <div className='mb-6 md:mb-0'>
+          <h5 className='uppercase mb-2 font-bold'>Quick Links</h5>
+          <Link to='' className='block mb-2 text-sm'>
+            Home
+          </Link>
+          <Link to='/directory' className='block mb-2 text-sm'>
+            Directory
+          </Link>
+          {/* Additional links */}
+        </div>
+        <div className='mb-6 md:mb-0'>
+          <h5 className='uppercase mb-2 font-bold'>Contact Us</h5>
+          <p className='text-white mb-4 text-sm'>250 North Bridge Road #11-01, Raffles City Tower Singapore 179101</p>
+          <p className='text-white mb-4 text-sm'>TEL +65 6828 8088</p>
+          <p className='text-white mb-4 text-sm'>FAX +65 6828 8090</p>
+          <div className='flex mt-4'>
+            <a href='https://www.facebook.com/vertexholdingsjapan/' target='_blank' rel='noopener noreferrer'>
+              <FontAwesomeIcon icon={faFacebook} className='mr-2' />
+            </a>
+            <a href='https://www.linkedin.com/company/vertex-holdings' target='_blank' rel='noopener noreferrer'>
+              <FontAwesomeIcon icon={faLinkedin} className='mr-2' />
+            </a>
+            <a href='mailto:communications@vertexholdings.com'>
+              <FontAwesomeIcon icon={faEnvelope} className='mr-2' />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='border-t border-white pt-4 text-white text-center text-xs'>
+        © 2019 by Vertex Holdings. All rights reserved. Legal
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
